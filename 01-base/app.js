9e43507922e5c72279530f4c4d0861ae698e41b5
Jan Philipp <zanfilipp259@gmail.com>
2024-06-20 17:21:11 +0300

    1

---
+++ 01-base/app.js
@@ -0,0 +1,35 @@
+let num = 42
+let firstName = 'Philipp'
+const isProgrammer = true
+
+// can do
+// let $ = 'test'
+// let $num = 42
+// let num$ = 42
+// let _ = 40
+
+// firstName = 'Mateo'
+// isProgrammer = false error  because of const
+
+// console.log(num + 10)
+// console.log(num - 10)
+// console.log(num * 10)
+// console.log(num / 10)
+// console.log(num % 10)
+
+// let num2 = num + 10
+
+// console.log(num2)
+
+// num = num + 1 
+// console.log(num)
+
+// let num3 = (num + 10) / 2
+// console.log(num3)
+
+const resultElement = document.getElementById('result')
+const res1 = document.getElementById('input1')
+const res2 = document.getElementById('input2')
+console.log(resultElement.textContent)
+resultElement.textContent = res1
+
